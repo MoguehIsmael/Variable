9eeec3239d807c99dbab828b3ebf7c6f5c2fe9c3
mogueh <ojhadkjhdoic@gmail.com>
2023-01-23 11:33:52 -0600

    Lab0 / Question 1

---
+++ Hello World/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Mogueh!");
+        String name = "Mogueh";
+        String nameInlowercase = name.toLowerCase();
+        System.out.println(nameInlowercase);
+
+    }
+}
